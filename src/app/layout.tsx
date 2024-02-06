7455711cf89602243a089bbcb2e3baa7e7e84390
Christian Blom <christian@christianblom.se>
2024-02-06 10:12:49 +0100

    Re-added Roboto font.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
-// import { Roboto_Condensed } from "next/font/google";
+import { Roboto_Condensed } from "next/font/google";
 import "./globals.scss";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-// const inter = Roboto_Condensed({ subsets: ["latin"] });
+const roboto = Roboto_Condensed({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Manifestor Retreat",
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body className={roboto.className}>
         <main className="flex min-h-screen flex-col items-center justify-between">
           <Header />
           {children}
