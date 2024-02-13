4290523349cff3c37143bb2a28afa98002b74272
Christian Blom <christian@christianblom.se>
2024-02-13 15:14:36 +0100

    Adjusted footer and replace CTA with custom button graphic.

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 import footerBackground from '../images/background-06.jpg';
 import creteMap from '../images/CreteMap.jpg';
 import cretePhotos from '../images/CretePic.png';
 import millaPavaka from '../images/MillaAndPavaka.png';
 import millaText from '../images/millaText.png';
 import pavakaText from '../images/pavakaText.png';
-import Link from "next/link";
+import moreInfoButton from '../images/more-info-button.png';
 
 function Footer() {
   return (
@@ -28,17 +29,15 @@ function Footer() {
       <div className="flex flex-col relative items-center mt-10">
         <h2><Link href="/about">Facilitating</Link></h2>
         <p>Milla Berglund and Pavaka Halel Katzir</p>
-        <Link href="/about"><Image src={millaPavaka} alt="Milla and Pavaka" className="sm:w-1/4" /></Link>
-        <p className="w-5/6 sm:w-3/4 text-center p-2 bg-white/70 shadow-[0_0_40px_0_rgba(255,255,255,1)] rounded-sm">
+        <Link className="inline" href="/about"><Image src={millaPavaka} alt="Milla and Pavaka" className="sm:w-2/3 lg:w-2/5 mx-auto" /></Link>
+        <p className="w-5/6 sm:w-3/4 lg:w-3/5 text-center p-2 bg-white/70 shadow-[0_0_40px_0_rgba(255,255,255,1)] rounded-sm">
           We met in Mary Ann Winiger&apos;s Immersions, and connected with ease and joy.
           We know the benefits of being together as Manifestors in the experiential field of investigating the Aura.
           The sameness of Aura is a bridge across languages, cultural conditioning and differences.
           This retreat is a natural happening out of this joy of being together, with our international tribe,
           celebrating both the familiarity of Manifestor attributes, and the uniqueness of each individual, in correctness.
         </p>
-        <h3 className="text-3xl text-center p-2 w-5/6 sm:w-auto bg-white/70 shadow-[0_0_40px_0_rgba(255,255,255,1)] rounded-sm m-5 mb-20">
-          <Link href="/register">Want more info on registering or the venue? Click here!</Link>
-        </h3>
+        <Link href="/register"><Image className="w-60 md:w-80" src={moreInfoButton} alt="For more info, click here!" /></Link>
       </div>
     </footer>
   )
