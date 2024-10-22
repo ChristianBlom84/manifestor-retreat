9d2ad830e81e8bb49cf6515b6e82a31575540470
Christian Blom <christian@christianblom.se>
2024-10-22 11:00:37 +0200

    Changed meta data in layout.tsx.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ const roboto = Roboto_Condensed({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'Manifestor Retreat',
-  description: 'Manifestor retreat in Crete',
+  description: 'Manifestor retreat in Barcelona',
 };
 
 export default function RootLayout({
